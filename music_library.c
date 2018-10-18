df2f5ee0b88a59930df9d56de842e23fe593a3e6
Maia Brydon <mbrydon@Maias-MacBook.local>
2018-10-18 11:47:43 -0400

    added music_library files

---
+++ music_library.c
@@ -0,0 +1,21 @@
+struct song_node * table[27];
+
+
+void add_song( char name[100], char artist[100]){
+}
+struct song_node *find_song( char name[100], char artist[100]){
+}
+void find_artist( char artist[100]){ //printf whether artist is found or not
+}
+void print_letter( char letter){
+}
+void print_artist_songs( char artist[100]){
+}
+void print_library(){
+}
+void shuffled_playlist(){
+}
+struct song_node *delete_song( char name[100], char artist[100]){
+}
+struct song_node *clear_library(){
+}
